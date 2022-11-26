09ade4121548f7c5f6301ba6ab778d18477f320d
HENDRICE_Rémy <r.hendrice@student.helmo.be>
2022-11-26 16:08:23 +0100

    LessonBloc n'avait pas été sauvegardé

---
+++ lib/blocs/join_lesson/join_lesson_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:studentlounge_mobile/blocs/join_lesson/join_lesson_events.dart';
+import 'package:studentlounge_mobile/blocs/join_lesson/join_lesson_state.dart';
+import 'package:studentlounge_mobile/models/lesson_model.dart';
+import 'package:studentlounge_mobile/repositories/lessons_repository.dart';
+
+class JoinLessonBloc extends Bloc<JoinLessonEvent, JoinLessonState> {
+  final Lesson lesson;
+  final LessonsRepository lessonsRepository;
+
+  JoinLessonBloc({required this.lesson, required this.lessonsRepository}) : super(JoinLessonInitial(lesson: lesson)) {
+    on<TryJoinLesson>((event, emit) {
+      _joinLesson();
+    });
+
+    on<TryLeaveLesson>((event, emit) {
+      _leaveLesson();
+    });
+  }
+
+  _leaveLesson() async {
+    emit(JoinLessonLoading(lesson: lesson));
+    Lesson result = await lessonsRepository.leaveLesson(lesson.id);
+    if(result.id == 0){
+      emit(LessonLeavable(lesson: lesson));
+    }else{
+      emit(LessonJoinable(lesson: lesson));
+    }
+  }
+
+  _joinLesson() async {
+    emit(JoinLessonLoading(lesson: lesson));
+    Lesson result = await lessonsRepository.joinLesson(lesson.id);
+    if(result.id == 0){
+      emit(LessonJoinable(lesson: lesson));
+    }else{
+      emit(LessonLeavable(lesson: lesson));
+    }
+  }
+}
