54063a8111675b3ae8dcba7ecc73925841c1d824
Kamal <k.ayadi@student.helmo.be>
2022-12-12 15:36:29 +0100

    style chat modify

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -16,11 +16,6 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   final TextEditingController message = TextEditingController();
 
-  final ScrollController scrollController = ScrollController(
-    initialScrollOffset: 0.0,
-    keepScrollOffset: true,
-  );
-
   @override
   Widget build(BuildContext context) {
     final fb = FirebaseFirestore.instance;
@@ -31,67 +26,71 @@ class _ChatPageState extends State<ChatPage> {
           title: Center(
               child: Text(widget.email,
                   style: const TextStyle(fontSize: 30, fontFamily: 'Gugi')))),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SingleChildScrollView(
-              reverse: true,
-              controller: scrollController,
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.72,
-                child: Messages(
-                  chatId: widget.chatId,
-                  name: widget.email,
-                ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            reverse: true,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: Messages(
+                chatId: widget.chatId,
+                name: widget.email,
               ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: message,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: theme.lightgrey,
-                      hintText: 'message',
-                      enabled: true,
-                      contentPadding: const EdgeInsets.only(
-                          left: 14.0, bottom: 8.0, top: 8.0),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: theme.activeIcon),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: const BorderSide(width: 0),
-                        borderRadius: BorderRadius.circular(10),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 60,
+                padding: const EdgeInsets.symmetric(vertical: 2.0),
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: message,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: theme.lightgrey,
+                          hintText: 'message',
+                          enabled: true,
+                          contentPadding: const EdgeInsets.only(
+                              left: 14.0, bottom: 8.0, top: 8.0),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: theme.activeIcon),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(width: 0),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {},
+                        onSaved: (value) {
+                          message.text = value!;
+                        },
                       ),
                     ),
-                    validator: (value) {},
-                    onSaved: (value) {
-                      message.text = value!;
-                    },
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (message.text.isNotEmpty) {
-                      fb.collection('messages${widget.chatId}').doc().set({
-                        'message': message.text.trim(),
-                        'time': DateTime.now(),
-                        'name': widget.email,
-                      });
+                    IconButton(
+                      onPressed: () {
+                        if (message.text.isNotEmpty) {
+                          fb.collection('messages${widget.chatId}').doc().set({
+                            'message': message.text.trim(),
+                            'time': DateTime.now(),
+                            'name': widget.email,
+                          });
 
-                      message.clear();
-                    }
-                  },
-                  icon: const Icon(Icons.send_sharp),
-                ),
-              ],
-            ),
-          ],
-        ),
+                          message.clear();
+                        }
+                      },
+                      icon: const Icon(Icons.send_sharp),
+                    ),
+                  ],
+                )),
+          )
+        ],
       ),
     );
   }
